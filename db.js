edef0c3ac52f2f9ec9ec737539e37fb4e5e14a8d
Sweety Sharma <sweetysharma@Sweetys-MacBook-Air.local>
2026-04-05 15:29:20 +0530

    changes in db file

--- db.js
+++ db.js
@@ -3,9 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
